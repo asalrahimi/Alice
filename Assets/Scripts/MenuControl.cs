a257fc14ac100baa7f713ec57455f57a6cd279ac
AsalRahimi <asalrahimi81@gmail.com>
2023-06-20 01:34:51 +0330

    file format fixed

--- Assets/Scripts/MenuControl.cs
+++ Assets/Scripts/MenuControl.cs
@@ -5,14 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class MenuControl : MonoBehaviour
 {
-   public void Click_Restart()
-    {
-      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }
+  public void Click_Restart()
+  {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+  }
 
-    public void Click_Start(){
+  public void Click_Start()
+  {
     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    
-    }
-
+  }
 }
