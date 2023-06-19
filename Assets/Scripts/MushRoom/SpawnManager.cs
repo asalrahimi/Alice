dc5b8641e61892c39fa68f85d8f095a9dd934ec8
AsalRahimi <asalrahimi81@gmail.com>
2023-06-20 01:31:59 +0330

    file format fixed

--- Assets/Scripts/MushRoom/SpawnManager.cs
+++ Assets/Scripts/MushRoom/SpawnManager.cs
@@ -5,79 +5,44 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-//mushroom 
+    //mushroom 
     [SerializeField] GameObject[] _objects;
-        [SerializeField] Camera _camera;
+    [SerializeField] Camera _camera;
     [SerializeField] int _offsetX;
-
     [SerializeField] int _offsetY;
 
     GameObject _spawnedObject;
-
     float _randomX;
-
     float _randomY;
 
-    public float[] x=new float[13];
-        public float[] y=new float[13];
-
-
-
- private float leftEdge;
-
+    public float[] x = new float[13];
+    public float[] y = new float[13];
+    private float leftEdge;
 
     void Start()
     {
-                Invoke("Spawn",7f);
-
-
+        Invoke("Spawn",7f);
     }
     
-
-    void Update()
-     
-
-        {
-      
-    }
-  
-
-
     void Spawn(){
+        x[0]=-1f;
+        x[1]=-41.16f;
+        x[2]=156.51f;
+        x[3]=18.25f;
+        x[4]=105f;
+        x[5]=55.01f;
 
-            x[0]=-1f;
-            x[1]=-41.16f;
-            x[2]=168.36f;
-           x[3]=152.7f;
-           x[4]=18.25f;
-           x[5]=99.59f;
-           x[6]=55.01f;
+        y[0]=-1.78f;
+        y[1]=0.07f;
+        y[2]=0.76f;
+        y[3]=-3.1f;
+        y[4]=-0.22f;
+        y[5]=-2.88f;
 
-            y[0]=-1.78f;
-            y[1]=0.07f;
-            y[2]=1.3f;
-            y[3]=0.76f;
-            y[4]=-3.1f;
-            y[5]=-0.07f;
-            y[6]=-2.88f;
-
-
-
-
-
-
-        for(int i=0;i<7;i++){
-
-        int RandomObjectId=Random.Range(0,_objects.Length);
-
-        Vector2 position=new Vector2(x[i],y[i]);
-
-        _spawnedObject=Instantiate(_objects[RandomObjectId],position,Quaternion.identity)as GameObject;
-
+        for(int i = 0;i < 5;i++){
+            int RandomObjectId=Random.Range(0,_objects.Length);
+            Vector2 position=new Vector2(x[i],y[i]);
+            _spawnedObject=Instantiate(_objects[RandomObjectId],position,Quaternion.identity)as GameObject;
         }
-
-    
-
     }
-
 }
