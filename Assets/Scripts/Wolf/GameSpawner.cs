0e18614fb8afb912e42f41610561a6475a862700
AsalRahimi <asalrahimi81@gmail.com>
2023-06-20 01:26:14 +0330

    file format fixed

--- Assets/Scripts/Wolf/GameSpawner.cs
+++ Assets/Scripts/Wolf/GameSpawner.cs
@@ -19,25 +19,22 @@ GameObject target;
     {
         SpawnWolfStart=GameObject.Find("SpawnWolfStart");
         SpawnWolfStop=GameObject.Find("SpawnWolfStop");
-
         target=GameObject.Find("Player");
         InvokeRepeating("SpawnEnemy",0f,5f);     
     }
 
     void SpawnEnemy()
     {
-        if(target.transform.position.x<=SpawnWolfStart.transform.position.x){
-        if(target.transform.position.x>=SpawnWolfStop.transform.position.x){
+        if (target.transform.position.x<=SpawnWolfStart.transform.position.x) {
+            if (target.transform.position.x>=SpawnWolfStop.transform.position.x)  {
 
                 //get random spawn point
                 randomSpawnPoint=Random.Range(0,spawnPoints.Length);
                 //get random enemy prefabs
                 randomEnemy=Random.Range(0,enemysPrefabs.Length);
                 //spawn it
-                Instantiate(enemysPrefabs[randomEnemy],spawnPoints[randomSpawnPoint].position,Quaternion.identity);}
-                
-
+                Instantiate(enemysPrefabs[randomEnemy],spawnPoints[randomSpawnPoint].position,Quaternion.identity);
+            }
         }
-       
     }
 }
