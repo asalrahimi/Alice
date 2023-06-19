70c67f60baa09aaab18752f621bda2275d7fc4ce
AsalRahimi <asalrahimi81@gmail.com>
2023-06-20 01:33:34 +0330

    monkey sound added , file format fixed , enemy location fixed

--- Assets/Scripts/Monkey/MonkeySpawnManager.cs
+++ Assets/Scripts/Monkey/MonkeySpawnManager.cs
@@ -8,63 +8,45 @@ public class MonkeySpawnManager : MonoBehaviour
     [SerializeField] GameObject[] _objects;
     [SerializeField] Camera _camera;
     [SerializeField] int _offsetX;
-
     [SerializeField] int _offsetY;
 
     GameObject _spawnedObject;
-
     float _randomX;
-
     float _randomY;
-    public float[] x=new float[6];
-    public float[] y=new float[6];
+    public float[] x = new float[6];
+    public float[] y = new float[6];
 
-
-
- private float leftEdge;
-
+    AudioSource _AudioPlayer;
+    public AudioClip Sound_Scream;
+    private float leftEdge;
 
     void Start()
     {
-                Invoke("Spawn",2f);
-
+        _AudioPlayer = GetComponent<AudioSource>();
+        Invoke("Spawn" , 2f);
     }
-    
-    void Update()
-     {   
-    }
-
-  
-
-
-    void Spawn(){
-
+      
+    void Spawn()
+    {
         x[0]=-17.91f;
         x[1]=-64.93f;
         x[2]=32.19f;
         x[3]=76.31f;
-        x[4]=114.99f;
+        x[4]=120.19f;
 
         y[0]=-0.45f;
         y[1]=0.03f;
         y[2]=-0.23f;
         y[3]=-0.23f;
-        y[4]=1.02f;
-
-
+        y[4]=2.26f;
 
         for(int i=0;i<5;i++){
+            int RandomObjectId=Random.Range(0,_objects.Length);
+            Vector2 position=new Vector2(x[i],y[i]);
+            _spawnedObject = Instantiate(_objects[RandomObjectId],position,Quaternion.identity)as GameObject;
 
-        int RandomObjectId=Random.Range(0,_objects.Length);
-
-        Vector2 position=new Vector2(x[i],y[i]);
-
-        _spawnedObject=Instantiate(_objects[RandomObjectId],position,Quaternion.identity)as GameObject;
-
+            //playing Monkey sound
+            _AudioPlayer.PlayOneShot(Sound_Scream);
         }
-
-
-    }
-
- 
+    } 
 }
