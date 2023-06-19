4799a3ce44dc7e4a5d5716af9bb50755f630dea8
AsalRahimi <asalrahimi81@gmail.com>
2023-06-19 21:02:37 +0330

    file format fixed + spell counter added

--- Assets/Scripts/ProjectalBehavior.cs
+++ Assets/Scripts/ProjectalBehavior.cs
@@ -4,24 +4,47 @@ using UnityEngine;
 
 public class ProjectalBehavior : MonoBehaviour
 {
-    public float speed=4.5f;
+    public float speed = 4.5f;
+    private static int spell;
+    public static int Spell {
+        get 
+        {
+            if (spell == null) {
+                return 0;
+            } else {
+                return spell;
+            }
+        } 
+        set 
+        {
+            if (value == null) {
+                spell = 0;
+            } else {
+                if (value > 3) {
+                    spell = 3;
+                } else if (value < 0) {
+                    spell = 0;
+                } else {
+                    spell = value;
+                }
+            }
+        }
+    }
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        spell = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-         transform.position-=transform.right*Time.deltaTime*speed;
-         Destroy(gameObject,1.0f);
-
+        transform.position -= transform.right * Time.deltaTime * speed;
+        Destroy(gameObject,1.0f);
     }
+
     private void OnTriggerEnter2D(Collider2D other){
         if (other.CompareTag("obstacle")) {
-            Debug.Log("hit detected");
             Destroy(gameObject);
         }
     }
