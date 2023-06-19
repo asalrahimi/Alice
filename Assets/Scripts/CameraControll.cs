0a7cae59325a85377ed7564779e9a33354bd20d2
AsalRahimi <asalrahimi81@gmail.com>
2023-06-20 01:38:47 +0330

    configuration of camera movements added

--- Assets/Scripts/CameraControll.cs
+++ Assets/Scripts/CameraControll.cs
@@ -23,13 +23,26 @@ public class CameraControll : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (target != null){
+        if (target != null) {
+            // default jump force
             Player.jumpforce = 7;
+
+            // update camera size
             camera.orthographicSize = cameraSize;
+
+            // get the last tree of the yello jungle to configure camera movements
             finishTree = GameObject.Find("FinishTree");
+
+            // get the tree that located before spell whole to configure camera movements
             afterSpellTree = GameObject.Find("AfterSpellTree");
+
+            // get the tree that located after spell whole to configure camera movements
             beforeSpellTree = GameObject.Find("BeforeSpellTree");
+
+            // get pink spell object
             pinkSpell = GameObject.Find("PinkSpell");
+
+            // if Alice is in the pink spell area increase the jump force and configure the camera location
             if ((target.transform.position.x <= (beforeSpellTree.transform.position.x + 5)) && (target.transform.position.x >= (afterSpellTree.transform.position.x))) {                
                 Player.jumpforce += 5;
                 if (tempY > (this.startY-3.5f)) {
@@ -40,17 +53,21 @@ public class CameraControll : MonoBehaviour
                     camera.orthographicSize = this.cameraSize;
                 } 
             } else {
+            // if Alice is out of the pink spell area reset the camera location to default
                 if (cameraSize > 5f) {
                     this.cameraSize -= 0.08f;
                     camera.orthographicSize = this.cameraSize;
                 } 
             }
 
+            // if Alice is going to enter the blue jungle , change the camera location because of different hights of jungles
             if (target.transform.position.x <= (finishTree.transform.position.x + 5)) {
                 if (tempY > (this.startY -4f)) {
                     tempY -= 0.05f;
                 }
             }
+
+            // setting the camera to follow Alice's movement on the X-axis
             transform.position = new Vector3(target.transform.position.x , tempY , transform.position.z);
         }
     }
