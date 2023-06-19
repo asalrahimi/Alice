3422ef4395fc381b1923e47c06cb279ec6c0a317
AsalRahimi <asalrahimi81@gmail.com>
2023-06-20 01:26:42 +0330

    file format fixed

--- Assets/Scripts/SpellGlass.cs
+++ Assets/Scripts/SpellGlass.cs
@@ -11,7 +11,6 @@ public class SpellGlass : MonoBehaviour
 
     public void UpdateSpellCount() 
     {
-        Debug.Log(ProjectalBehavior.Spell);
         textProObj = GameObject.Find("PlayerSpellCount");
         collectedSpellTxt = textProObj.GetComponent<TextMeshProUGUI>();
         collectedSpellTxt.text = ProjectalBehavior.Spell.ToString();
@@ -19,13 +18,13 @@ public class SpellGlass : MonoBehaviour
 
     public static void Collect(Collision2D collision) 
     {
-        //find current spell
+        // find current spell
         spell = GameObject.Find(collision.transform.name);
 
-        //destroy current spell after collision
+        // destroy current spell after collision
         Destroy(spell);
 
-        //update count of collected spell
+        // update count of collected spell
         ProjectalBehavior.Spell += 1;
         SpellGlass spellIns = new SpellGlass();
         spellIns.UpdateSpellCount();
