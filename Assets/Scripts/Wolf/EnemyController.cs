04cc5da093eeb0702d81fdd6a472ba904102029b
AsalRahimi <asalrahimi81@gmail.com>
2023-06-19 20:51:26 +0330

    file format fixed

--- Assets/Scripts/Wolf/EnemyController.cs
+++ Assets/Scripts/Wolf/EnemyController.cs
@@ -4,48 +4,31 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    Rigidbody2D rb;
+    GameObject target;
+    GameObject SpawnWolfStart;
+    float moveSpeed;
+    Vector2 directionToTarget;
+    private float leftEdge;
 
-
-//now lets maks them follow the player
-
-Rigidbody2D rb;
-GameObject target;
-//Player player;
-
-GameObject SpawnWolfStart;
-
-float moveSpeed;
-Vector2 directionToTarget;
-private float leftEdge;
-    public void Start()
-    {
-
-    }
     public void Update()
     {     
         MoveEnemy();        
     }
-    public void MoveEnemy(){
 
-         SpawnWolfStart=GameObject.Find("SpawnWolfStart");
-         target=GameObject.Find("Player");
-       
-
-         rb=GetComponent<Rigidbody2D>();        
-
-
-            if(target!=null){
-                if(target.transform.position.x<=SpawnWolfStart.transform.position.x){
-                    directionToTarget=((target.transform.position-transform.position).normalized);
-                    rb.velocity=new Vector3(directionToTarget.x,directionToTarget.y-2,0);
-
-                    transform.position +=Vector3.right*Time.deltaTime*2;
-                                 }
-                            }
-
-                else{
-                     rb.velocity=Vector3.zero;      
-                    }
-        
+    public void MoveEnemy()
+    {
+        SpawnWolfStart = GameObject.Find("SpawnWolfStart");
+        target = GameObject.Find("Player");
+        rb = GetComponent<Rigidbody2D>();        
+        if(target != null){
+            if(target.transform.position.x <= SpawnWolfStart.transform.position.x){
+                directionToTarget=((target.transform.position-transform.position).normalized);
+                rb.velocity = new Vector3(directionToTarget.x,directionToTarget.y-2,0);
+                transform.position +=Vector3.right*Time.deltaTime*2;
+                }
+        } else {
+            rb.velocity=Vector3.zero;      
+        }
     }
 }
