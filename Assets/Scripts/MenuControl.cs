cdaed406853f5a5327c73c1bb9d6998550041886
yasamanseyfollahiasl <75227157+yasamanseyfollahiasl@users.noreply.github.com>
2023-06-20 06:59:57 +0430

    Last Version

--- Assets/Scripts/MenuControl.cs
+++ Assets/Scripts/MenuControl.cs
@@ -5,13 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class MenuControl : MonoBehaviour
 {
-  public void Click_Restart()
-  {
-    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-  }
+    public void Click_Restart()
+    {
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 
-  public void Click_Start()
-  {
+    public void Click_Next_Level(){
     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-  }
+    
+    }
 }
